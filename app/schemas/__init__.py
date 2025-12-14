346cc430a252ad36ce3ff9c918d8f36a8f71c855
Trisha K <168080442+triiJU@users.noreply.github.com>
2025-12-14 17:15:02 +0530

    Import schemas and define __all__ in __init__.py

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,1 +1,9 @@
+from app.schemas.page import PageResponse, PageListResponse
+from app.schemas.post import PostResponse, PostListResponse
+from app.schemas.user import UserResponse, UserListResponse
 
+__all__ = [
+    "PageResponse", "PageListResponse",
+    "PostResponse", "PostListResponse",
+    "UserResponse", "UserListResponse"
+]
