b9d356a20b254ced97d032d43fc8e5a4776e3d4a
Trisha K <168080442+triiJU@users.noreply.github.com>
2025-12-14 17:14:01 +0530

    Import models and define __all__ for exports

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,1 +1,5 @@
+from app.models.page import PageModel
+from app.models.post import PostModel
+from app.models.user import SocialMediaUserModel
 
+__all__ = ["PageModel", "PostModel", "SocialMediaUserModel"]
